b49fbca7711d04b5e4ae31e964ea3d28afdc3d43
Diana Agapova <d18102002@mail.ru>
2024-04-24 17:35:08 +0300

    add test class Bar

---
+++ tests/QuotesTests/BarTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Quotes;
+
+namespace QuotesTests;
+
+public class BarTest
+{
+    [Fact]
+    public void ToString_ShouldReturnBarInCSVFormat()
+    {
+        // Arrange
+        var bar = new Bar()
+        {
+            Symbol = "ABBV",
+            Description = "NYSE",
+            Date = new DateOnly(2020, 1, 02),
+            Time = new TimeOnly(8, 1, 0),
+            Open = 89.090m,
+            High = 89.090m,
+            Low = 88.950m,
+            Close = 88.950m,
+            TotalVolume = 1325
+        };
+
+        // Act
+        var line = bar.ToString();
+
+        // Assert
+        line.Should().BeEquivalentTo("ABBV,NYSE,02.01.2020,8:01,89.090,89.090,88.950,88.950,1325");
+    }
+}
